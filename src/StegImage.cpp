0e1425a87ff1b18e28963dd64bb6d141ffcec4e8
Micah <micah_schiewe1@baylor.edu>
2020-04-13 16:20:01 -0500

    First version of get.

--- src/StegImage.cpp
+++ src/StegImage.cpp
@@ -15,8 +15,46 @@ StegImage::StegImage(string) {
 }
 
 char StegImage::get() {
-    // TODO
-    return '\0';
+    char result = '\0';
+    int bitsRemaining = CHAR_BIT;
+
+    char buffer;
+    int bitCntr;
+
+    // Create a mask to isolate the high-order bit
+    char mask = 0x1;
+    mask <<= CHAR_BIT - 1;
+    mask = ~mask;
+
+    // Compute whether a certain number of bytes need to be skipped.
+    const int BYTES_TO_SKIP = (bitDepth / CHAR_BIT) - 1;
+    const int DEPTH = bitDepth > CHAR_BIT ? CHAR_BIT : bitDepth;
+
+    while (bitsRemaining > 0) {
+        // Read byte with encoded data
+        if (BYTES_TO_SKIP > 0) {
+            file.seekp(BYTES_TO_SKIP, ios::cur);
+            file.seekg(BYTES_TO_SKIP, ios::cur);
+        }
+        buffer = file.get();
+
+        // Prepare to read
+        buffer <<= bitDepth - 1;
+        bitCntr = CHAR_BIT - bitDepth + 1;
+
+        // Read bits out of this byte
+        do {
+            // Pre-decrement, then extract the bit
+            bitsRemaining--;
+            result |= (buffer & mask) >> bitsRemaining;
+
+            // Prepare to extract the next bit
+            buffer <<= bitDepth;
+            bitCntr -= bitDepth;
+        } while (bitCntr >= 0);
+    }
+
+    return result;
 }
 
 void StegImage::put(char) {
